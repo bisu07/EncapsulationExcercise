668f1ba052372ba2ccba3c495a60e22a42189304
bisu07 <g26.bishal@gmail.com>
2021-10-13 21:46:48 -0500

    this is initial commit

---
+++ EncapsulationExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+            Console.WriteLine($"Enter the amount of you like to deposit:");
+            var amountDeposit = double.Parse(Console.ReadLine());
+            account.Deposit(amountDeposit);
+            Console.WriteLine($"Your available deposit amount is:{account.GetBalance()}");
+            //account.Balance = 1000;
+
+
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
